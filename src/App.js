a48fa9a2e04722d35b2444cd51cdebc438aa3adf
codenutri <codenutri88@gmail.com>
2023-03-12 10:25:05 +0000

    cahnged app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Well, I was changed!
+          Well, I was changed on the feature fridge!
         </p>
         <a
           className="App-link"
